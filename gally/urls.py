7ff9a6d8ffc3968b878dea14f009d5f7482c49e3
IngabireTina <ingabire067@gmail.com>
2021-04-13 17:52:02 -0400

    added home view function

--- gally/urls.py
+++ gally/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('img/',views.addImage, name= 'image-new'),
+    path('', home, name= 'home'),
 ]
--- gally/views.py
+++ gally/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+
+
+def home(request):
+    image = Images.objects.all()
+    locations = Location.get_locations()
+    category = Category.objects.all()
+    context = {'image':image, 'locations': locations, 'category':category}
+    return render(request, 'home.html', context )
