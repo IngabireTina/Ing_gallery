a1c171d2d77e2ce72e14646bedbce7ccfd59157a
IngabireTina <ingabire067@gmail.com>
2021-04-13 17:42:45 -0400

    added setup test

--- gally/tests.py
+++ gally/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+
+class EditorTestClass(TestCase):
+
+    def setUp(self):
+        self.location = Location.objects.create(name='Kigali')
+        self.category = Category.objects.create(name='park')
+        self.park= Image(name = 'park', link = 'https', description ='hhhhhhhh',  category = self.category, location = self.location)
+ 
