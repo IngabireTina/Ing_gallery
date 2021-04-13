29a2ae9272085a7125b19376ed88c03b77dbea75
IngabireTina <ingabire067@gmail.com>
2021-04-13 08:51:02 -0400

    added category image

--- gally/models.py
+++ gally/models.py
@@ -3,9 +3,12 @@ from ingallery import settings
 
 # Create your models here.
 
+CATEGORY = (('TRAVEL','TRAVEL'),('PARTIES','PARTIES'))
+
 class Images(models.Model):
     name = models.CharField(max_length=60)
     details = models.TextField()
+    img_category = models.CharField(max_length=100, choices=CATEGORY, default='PARTIES')
     photo = models.ImageField(upload_to='galy')
 
     def __str__(self):
